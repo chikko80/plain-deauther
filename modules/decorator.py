d8df75d33f9252903ac888c57adfb1a7dfb500f2
chikko <seitzmanuel@t-online.de>
2020-11-25 14:52:52 +0000

    push mobile test

--- modules/decorator.py
+++ modules/decorator.py
@@ -25,10 +25,11 @@ def __print_banner():
 def mobile_banner():
     print(
         """
-_____ _____ _____ _____ _____ _____ _____ _____ 
-|  _  |   __|  _  |  |  |_   _|  |  |   __| __  |
-|   __|   __|     |  |  | | | |     |   __|    -|
-|__|  |_____|__|__|_____| |_| |__|__|_____|__|__|
+,------.,------. ,---. ,--. ,--,--------,--.  ,--,------,------.  
+|  .--. |  .---'/  O  \|  | |  '--.  .--|  '--'  |  .---|  .--. ' 
+|  '--' |  `--,|  .-.  |  | |  |  |  |  |  .--.  |  `--,|  '--'.' 
+|  | --'|  `---|  | |  '  '-'  '  |  |  |  |  |  |  `---|  |\  \  
+`--'    `------`--' `--'`-----'   `--'  `--'  `--`------`--' '--' 
         """
     )
 
@@ -36,7 +37,7 @@ def base_menu(color):
     def function_wrapper(func):
         def draw_outlines(*args):
             self = args[0]
-            __print_banner()
+            mobile_banner()
             __print_colored_line(color)
             if not self.manager.chosen_interface:
                 cprint('Please select an interface:','yellow')
@@ -55,7 +56,7 @@ def base_menu(color):
                 mac = colored(f'{self.manager.chosen_interface.mac_address}',mac_color)
                 state_label = colored('State: ',menu_color)
                 state = colored(f'{self.manager.chosen_interface.state}',state_color)
-                print(f"{si_label}{si}{seperator}{mode_label}{mode}{seperator}{mac_label}{mac}{seperator}{state_label}{state}")
+                print(f"{si_label}{si}{seperator}{mode_label}\n{mode}{seperator}{mac_label}{mac}{seperator}\n{state_label}{state}")
                 # clean line,otherwise their will be a new line because string is at line limit
                 print ("\033[A                                                                                      \033[A")
             __print_colored_line(color)
@@ -64,9 +65,44 @@ def base_menu(color):
         return draw_outlines
     return function_wrapper
 
-def __print_colored_line(color):  
-    cprint("--------------------------------------------------------------------------------------",color)
+# def base_menu(color):
+#     def function_wrapper(func):
+#         def draw_outlines(*args):
+#             self = args[0]
+#             __print_banner()
+#             __print_colored_line(color)
+#             if not self.manager.chosen_interface:
+#                 cprint('Please select an interface:','yellow')
+#             else:
+#                 menu_color = 'cyan'
+#                 interface_color = 'red'
+#                 mode_color = 'red'
+#                 mac_color = 'red'
+#                 state_color = 'red'
+#                 seperator = colored(" | ",menu_color)
+#                 si_label = colored('Selected interface: ',menu_color)
+#                 si = colored(f'{self.manager.chosen_interface.interface}',interface_color)
+#                 mode_label = colored('Mode: ',menu_color)
+#                 mode = colored(f'{self.manager.chosen_interface.mode}',mode_color)
+#                 mac_label = colored('MAC: ',menu_color)
+#                 mac = colored(f'{self.manager.chosen_interface.mac_address}',mac_color)
+#                 state_label = colored('State: ',menu_color)
+#                 state = colored(f'{self.manager.chosen_interface.state}',state_color)
+#                 print(f"{si_label}{si}{seperator}{mode_label}{mode}{seperator}{mac_label}{mac}{seperator}{state_label}{state}")
+#                 # clean line,otherwise their will be a new line because string is at line limit
+#                 print ("\033[A                                                                                      \033[A")
+#             __print_colored_line(color)
+#             func(*args)
+#             __print_colored_line(color)
+#         return draw_outlines
+#     return function_wrapper
 
+
+# def __print_colored_line(color):  
+#     cprint("--------------------------------------------------------------------------------------",color)
+
+def __print_colored_line(color):  
+    cprint("-----------------------------------------------------------------",color)
 
 # def menu_outline_decorator(color):
 #     def function_wrapper(func):
